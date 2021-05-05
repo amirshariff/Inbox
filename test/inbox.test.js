445f4e6a5f43ce83bfa7450bccec4ab6ebf950fe
Amir Shariff <amirshariff@msn.com>
2021-05-05 07:56:42 +0300

    testing set

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,5 +1,19 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
 const web3 = new Web3(ganache.provider());
+
+let accounts ;
+
+beforeEach(async () => {
+    //get a list of accounts
+    accounts = await web3.eth.getAccounts()
+        
+})
+
+
+describe('Inbox', () => {
+    it('deploys a contracts', () => {
+        console.log(accounts)
+    })
+});
